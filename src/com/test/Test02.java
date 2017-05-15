5d1a21a0b6dea5e77661d0cfceab5d5de943a601
eddywon <2631992008@qq.com>
2017-05-15 17:12:51 +0800

    test revert five!

--- src/com/test/Test02.java
+++ src/com/test/Test02.java
@@ -1,5 +1,7 @@
 package com.test;
 
+import java.util.Date;
+
 /**
  * Created by Eddy on 2017/5/15 0015.
  */
@@ -7,4 +9,5 @@ public class Test02 {
 	int i = 10;
 	int l = 40;
 	int x = 50;
+	Date date = new Date();
 }
