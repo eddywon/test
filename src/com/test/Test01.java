b8a6ab18c84cdbdc54642d49d481c046490b6dd7
eddy <2631992008@qq.com>
2017-05-23 09:26:18 +0800

    打印输出

--- src/com/test/Test01.java
+++ src/com/test/Test01.java
@@ -4,5 +4,9 @@ package com.test;
  * Created by Administrator on 2017/5/15 0015.
  */
 public class Test01 {
-	public String s = "hello";
+	public static String s = "hello";
+
+	public static void main(String[] args){
+		System.out.println(s);
+	}
 }
